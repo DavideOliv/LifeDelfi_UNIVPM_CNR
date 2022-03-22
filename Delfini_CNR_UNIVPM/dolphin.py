ab97c496a4acecdb43805d8271c551f399e50616
Antonio Lanciotti <antonio.lanciotti@live.it>
2022-03-22 14:12:41 +0100

    Update

--- Delfini_CNR_UNIVPM/dolphin.py
+++ Delfini_CNR_UNIVPM/dolphin.py
@@ -11,17 +11,16 @@ else:
 
 sr,wav = wavfile.read(file)
 N = wav.shape[0]
-t = np.arange(N) / sr
 
 low_band = 35e3
 high_band = 50e3
 filter = sg.butter(4, [low_band, high_band], btype='bandpass', analog=False, output='sos', fs=sr)
 wav = sg.sosfilt(filter, wav)
 
-f, t, Sxx = sg.spectrogram(wav, sr)
+_, _, Sxx = sg.spectrogram(wav, sr)
 
 onda_tot = np.sum(Sxx, axis=0)
-peaks, props = sg.find_peaks(onda_tot, height=np.mean(onda_tot)*3, distance=50)
+peaks, _ = sg.find_peaks(onda_tot, height=np.mean(onda_tot)*3, distance=50)
 
 click_freq_min = 5
 if peaks.shape[0]>=(N/sr*click_freq_min):
--- Delfini_CNR_UNIVPM/dolphin_detecter.py
+++ Delfini_CNR_UNIVPM/dolphin_detecter.py
@@ -22,42 +22,42 @@ res = np.empty(FREQUENCY * SECONDS, dtype=np.int16)
 
 
 def check(wav):
-	wav = sg.sosfilt(filter, res)
-	_, _, Sxx = sg.spectrogram(wav, FREQUENCY)
-	
-	onda_tot = np.sum(Sxx, axis=0)
-	peaks, props = sg.find_peaks(onda_tot, height=np.mean(onda_tot)*3, distance=50)
+    wav = sg.sosfilt(filter, res)
+    _, _, Sxx = sg.spectrogram(wav, FREQUENCY)
+    
+    onda_tot = np.sum(Sxx, axis=0)
+    peaks, props = sg.find_peaks(onda_tot, height=np.mean(onda_tot)*3, distance=50)
 
-	if peaks.shape[0] >= (MIN_SHAPE_DETECT):
-	    return True
-	else:
-	    return False
+    if peaks.shape[0] >= (MIN_SHAPE_DETECT):
+        return True
+    else:
+        return False
 
 def main():
-	line = p.stdout.read(N_BYTES)
-	while line:
-		residx = 0
-		for idx in range(0, len(line), 2):
-			val = int.from_bytes(line[idx:idx+2], "little", signed=True)
+    line = p.stdout.read(N_BYTES)
+    while line:
+        residx = 0
+        for idx in range(0, len(line), 2):
+            val = int.from_bytes(line[idx:idx+2], "little", signed=True)
 			
-			res[residx] = val
-			residx += 1	
-		
-		dolphin = check(res)
-		if dolphin:
-			print("DELFINO SI")
-		else:
-			print("DELFINO NO")
+            res[residx] = val
+            residx += 1    
+        
+        dolphin = check(res)
+        if dolphin:
+            print("DELFINO SI")
+        else:
+            print("DELFINO NO")
 
-		line = p.stdout.read(N_BYTES)
-		
+        line = p.stdout.read(N_BYTES)
+        
 if __name__ == "__main__":
-	p = Popen(f'arecord -f S16_LE -r {FREQUENCY} -c 1 -t raw', stdout=PIPE, shell=True)
-	try:
-		main()
-	except KeyboardInterrupt:
-		print("Keybord Interrupt")
-	finally:
-		p.terminate()
-		print("arecord subprocess terminated")
-	
+    p = Popen(f'arecord -f S16_LE -r {FREQUENCY} -c 1 -t raw', stdout=PIPE, shell=True)
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Keybord Interrupt")
+    finally:
+        p.terminate()
+        print("arecord subprocess terminated")
+    
