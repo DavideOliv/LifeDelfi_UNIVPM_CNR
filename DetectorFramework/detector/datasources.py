eddd52753419ce055d0d6fe7a56b7cdf90602164
Antonio Lanciotti <antonio.lanciotti@live.it>
2022-05-11 20:26:33 +0200

    Aggiunto data sources jack rocco (non testato)

--- DetectorFramework/detector/datasources.py
+++ DetectorFramework/detector/datasources.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import socket
 import numpy as np
 import subprocess
 import scipy.io.wavfile
@@ -53,3 +55,27 @@ class WAVFileDataSource(DataSource):
             return None, None
         else:
             return chunk, {"offset":oldoffset/self.sr, "wave": bytearray(chunk)}
+
+class JackDataSource(DataSource):
+    def __init__(self, host="127.0.0.1", port = 8888):
+        size_of_float = 4
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((host, port))
+        self.socket.sendall(b"nframes")
+        self.nframes = int(self.socket.recv(256).decode("utf8").split("\n")[0])
+        self.socket.sendall(b"len")
+        self.nblocks = int(self.socket.recv(256).decode('utf8').split("\n")[0])
+        self.socket.sendall(b"rate")
+        self.samplerate = int(self.socket.recv(256).decode('utf8').split("\n")[0])
+        self.socket.sendall(b"seconds")
+        self.seconds = int(self.socket.recv(256).decode('utf8').split("\n")[0])
+        self.blocksize = size_of_float * self.nframes
+        self.socket.sendall(b"dump")
+        self.i = -1
+
+    def getChunk(self):
+        if self.i >= self.nblocks:
+            return None, None
+        chunk = np.frombuffer(s.recv(self.blocksize), dtype=np.float32)
+        self.i += 1
+        return chunk, {"offset":self.i/self.samplerate, "wave": chunk}
